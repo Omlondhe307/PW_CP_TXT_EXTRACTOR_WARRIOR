fffba15dec83495ec5699a5251e6fa29994fea62
bhanupratapsingh25 <doremontv040@gmail.com>
2025-05-10 17:56:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-api_id = "23975040"
-api_hash = "70dfb28db53ea9a0bc2a7cd8dd66dce1"
-bot_token = "8073031199:AAEUNy9RWb93YKzGy_U8VZJnJQkbLyTl5EQ"
-auth_users = [7669049168]
+api_id = "8d0fe8b5ae8149455681681253b2ef17"
+api_hash = "21601817"
+bot_token = ""
+auth_users = [6764661699]
