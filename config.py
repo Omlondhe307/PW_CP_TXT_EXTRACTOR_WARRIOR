3bc3c619289fcee3a73c481a69c37c0bbf99a288
golu001-glory <pafoda9953@jazipo.com>
2025-05-10 17:00:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 api_id = "23975040"
 api_hash = "70dfb28db53ea9a0bc2a7cd8dd66dce1"
 bot_token = "8073031199:AAEUNy9RWb93YKzGy_U8VZJnJQkbLyTl5EQ"
-auth_users = [7669049168,1767915668]
+auth_users = [7669049168]
